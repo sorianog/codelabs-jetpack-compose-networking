b10e8c13d9e659de36ebda8fe2c9bac59ceb462c
sorianog <13261711+sorianog@users.noreply.github.com>
2025-05-05 17:26:38 -0700

    Completed Step 6: Update unit test

--- app/src/test/java/com/example/marsphotos/MarsViewModelTest.kt
+++ app/src/test/java/com/example/marsphotos/MarsViewModelTest.kt
@@ -22,8 +22,7 @@ class MarsViewModelTest {
                 marsPhotosRepository = FakeRemoteMarsPhotosRepository()
             )
             assertEquals(
-                MarsUiState.Success("Success: ${FakeDataSource.photosList.size} Mars " +
-                        "photos retrieved"),
+                MarsUiState.Success(FakeDataSource.photosList),
                 marsViewModel.marsUiState
             )
         }
